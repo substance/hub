902804c679a47405572fc865ed0f1d644941e6f8
Michael Aufreiter <michael@substance.io>
2016-01-10 12:47:27 +0100

    Provide a valid Note document as seed.

--- db/seeds/01_default_changes.js
+++ db/seeds/01_default_changes.js
@@ -1,18 +1,40 @@
+var change = {
+  ops: [
+    {
+      type: "create",
+      path: ["p1"],
+      val: {
+        id: "p1",
+        type: "paragraph",
+        content: "Substance is a JavaScript library for web-based content editing. Build simple text editors or full-featured publishing systems. Substance provides you building blocks for your very custom editor.",      
+      }
+    },
+    {
+      type: "create",
+      path: ["t1"],
+      val: {
+        id: "t1",
+        type: "todo",
+        done: false,
+        content: "Water the plants"
+      }
+    },
+    {
+      type: "set",
+      path: ['body', 'nodes'],
+      original: [],
+      val: ["p1", "t1"],
+    }
+  ]
+};
+
 var records = [
   {
-    id: "doc-15/master/0",
-    changeset: "doc-15/master",
+    id: "test-note/master/0",
+    changeset: "test-note/master",
     pos: 0,
-    data: '{"a":"b","b":"c"}',
+    data: JSON.stringify(change),
     timestamp: 1450636440163,
-    user: "user-1"
-  },
-  {
-    id: "doc-15/master/1",
-    changeset: "doc-15/master",
-    pos: 1,
-    data: '{"a":"b","b":"c"}',
-    timestamp: 1450636442328,
     user: "user-1"
   }
 ];
